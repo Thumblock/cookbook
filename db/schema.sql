de535763cdf4a9fb295d362f92542dd55c81381b
Chipp Larusson <chipp.larusson@hotmail.com>
2025-11-09 16:42:17 +0100

    Added two more tables : Recipe table and recipe_ingredient junction table.

--- db/schema.sql
+++ db/schema.sql
@@ -16,3 +16,21 @@ CREATE TABLE ingredient (
     name TEXT NOT NULL UNIQUE,
     category TEXT
 );
+
+-- Recipes
+CREATE TABLE recipe (
+    recipe_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    title TEXT NOT NULL,
+    instructions TEXT NOT NULL,
+    created_by UUID REFERENCES cookbook_user(user_id),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+-- Recipe_Ingredient junction
+CREATE TABLE recipe_ingredient (
+    recipe_id UUID NOT NULL REFERENCES recipe(recipe_id) ON DELETE CASCADE,
+    ingredient_id INT NOT NULL REFERENCES ingredient(ingredient_id),
+    quantity NUMERIC(10,2),
+    unit TEXT,
+    PRIMARY KEY (recipe_id, ingredient_id)
+);
